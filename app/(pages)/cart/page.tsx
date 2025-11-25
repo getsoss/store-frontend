8bde46bd7a92ef41112c4bca867db860a1615174
getsoss <getsoss84@gmail.com>
2025-11-25 10:53:18 +0900

    [Feat] 장바구니에서 상품 삭제

--- app/(pages)/cart/page.tsx
+++ app/(pages)/cart/page.tsx
@@ -180,6 +180,7 @@ export default function CartPage() {
         },
         body: JSON.stringify({
           productId: item.productId,
+          productSizeId: item.productSizeId,
         }),
       });
 
